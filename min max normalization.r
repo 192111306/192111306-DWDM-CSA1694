43b63600d2fe895aacc18d3241348a602e87e89c
Jamalamma <112162566+Jamalamma@users.noreply.github.com>
2023-05-10 20:50:25 +0530

    Add files via upload

---
+++ min max normalization.r
@@ -0,0 +1,6 @@
+original_vector=c(10,20,30,40,50)
+normalized_vector=(original_vector-min(original_vector))/(max(original_vector)-min(original_vector))
+print(normalized_vector)
+original_vector=c(100,200,300,400,10,20,40,50)
+normalized_vector=(original_vector-min(original_vector))/(max(original_vector)-min(original_vector))
+print(normalized_vector)
